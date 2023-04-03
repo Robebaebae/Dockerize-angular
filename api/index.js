804ee44f3af871008d1661eacb57c868b338ecc1
Robert Timbang Quintero <90501105+Robebaebae@users.noreply.github.com>
2023-04-03 18:41:17 +0800

    first bunura lahat

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    db.query('SELECT * FROM my_table', (error, results) => {
+    db.query('SELECT * FROM user', (error, results) => {
         if (error) {
             console.error('Error querying database:', error);
             res.sendStatus(500);
